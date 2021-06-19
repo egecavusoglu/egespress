d394e21c229398a401c1ab897f81691d843e5cc9
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-18 20:52:40 -0700

    Added routing middleware.

--- Readme.md
+++ Readme.md
@@ -6,5 +6,9 @@ A boilerplate to scaffold powerful koajs apps for API development.
 
 - [x] Request timer middleware
 - [x] Global Error handling middleware
+- [ ] Authentication middleware
+- [ ] CORS middleware
+- [ ] Rate limiting middleware (DDos)
 - [ ] Testing
 - [ ] CI/CD options
+- [ ] Add GraphQL endpoint?
--- src/app/index.ts
+++ src/app/index.ts
@@ -2,14 +2,9 @@ import Koa from "koa";
 import Router from "koa-router";
 import errorHandler from "../error-handler";
 import timer from "../timer";
+import router from "../routes";
 
 const app = new Koa();
-const router = new Router();
-
-router.get("/", async (ctx: Koa.Context) => {
-  ctx.status = 200;
-  ctx.body = "API is live.";
-});
 
 // Timing middleware (important that this is the first middleware to have more accurate measurements.)
 app.use(timer);
