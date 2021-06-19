65c6670a9c41ffc59c21022bddb7c4b096f5df87
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-18 21:05:51 -0700

    Added initial testing framework.

---
+++ src/routes/auth/index.ts
@@ -0,0 +1,11 @@
+import Koa from "koa";
+import Router from "koa-router";
+
+const router = new Router();
+
+router.get("/", (ctx: Koa.Context, next: Koa.Next) => {
+  ctx.status = 200;
+  ctx.body = "Auth Endpoint";
+});
+
+export default router;
---
+++ test/index.test.js
@@ -0,0 +1,3 @@
+it("Your first dummy test!", () => {
+  expect(1).toBe(1);
+});
